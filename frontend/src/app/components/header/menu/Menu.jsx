192ba7b55e2ec657be40423b0805968b0a854dec
GK <gabykassab0@gmail.com>
2025-02-05 22:45:35 +0200

    adding logo

--- frontend/src/app/components/header/menu/Menu.jsx
+++ frontend/src/app/components/header/menu/Menu.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+"use client";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react'
+import logo from './../../../public/images/logo.png'
 const Menu = () => {
+
+    const [fixedHeader,setFixedHeader] = useState(false)
+
   return (
-    <div>
-      Menu
-    </div>
-  )
+<>
+<div className={`header-menu bg-white ${fixedHeader ? "fixed" : ''} ` }>
+
+<div className='container flex items-center justify-between h-20'>
+    <Link className='menu-left-block' href="/">
+        <Image src={logo} width={2000} height={1000} alt='logo' priority={true} className='w-[149px] max-sm:w-[132px]' />
+    </Link>
+</div>
+</div>
+</>
+)
 }
 
 export default Menu
