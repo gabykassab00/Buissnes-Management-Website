0eeb4d4fbfcd4bc36d41ff06937650c9112886b6
GK <gabykassab0@gmail.com>
2025-02-06 17:56:29 +0200

    adding swiper function

--- frontend/src/app/components/slider/Slider.jsx
+++ frontend/src/app/components/slider/Slider.jsx
@@ -1,10 +1,26 @@
+"use client";
 import React from 'react'
-
+import {Swiper,SwiperSlide} from 'swiper/react'
+import {Autoplay,Navigation,Pagination} from 'swiper/modules'
+import * as Icon from '@phosphor-icons/react/dist/ssr'
 const Slider = () => {
   return (
-    <div>
-      a
+    <>
+    <div className='slider-block'>
+        <div className='prev-arrow items-center justify-center '>
+            <Icon.CaretLeft className='text-white heading6' weight='bold'/>
+        </div>
+
+        <div className='slider-main '>
+            <Swiper spaceBetween={0} slidesPerView={1} navigation={{prevEl:'.prev-arrow',nextEl:'.next-arrow'}} loop={true} pagination={{clickable:true}} speed={400} modules={[Pagination,Autoplay,Navigation]} className='h-full relative' autoplay={{delay:4000}}>
+                <SwiperSlide>
+                    
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
     </div>
+      </>
   )
 }
 
