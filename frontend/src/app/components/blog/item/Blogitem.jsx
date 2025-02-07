fc3b73e8d65d2b7c278b675c614e40bdf86945d9
GK <gabykassab0@gmail.com>
2025-02-07 23:33:07 +0200

    adding title and date

--- frontend/src/app/components/blog/item/Blogitem.jsx
+++ frontend/src/app/components/blog/item/Blogitem.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import * as Icon from '@phosphor-icons/react/dist/ssr'
 
 const Blogitem = ({data}) => {
   return (
@@ -14,6 +15,20 @@ const Blogitem = ({data}) => {
         <div className='infor sm:p-6 p-4'>
             <div className='caption2 py-1 px-3 bg-surface rounded-full inline-block capitalize bg-blue-100'>
                 {data.category} 
+            </div>
+
+            <div className='heading6 mt-2'>
+                {data.title}
+            </div>
+            <div className='date flex item-center gap-4 mt-2'>
+                <div className='author caption2 text-secondary'>
+                    by <span className='text-on-surface'>{data.author}</span>
+                </div>
+                <div className='item-date flex items-center'>
+                    <Icon.CalendarBlank weight='bold'/>
+                    <span className='ml-1 caption2'>{data.date}</span>
+                </div>
+
             </div>
         </div>
         </Link>
