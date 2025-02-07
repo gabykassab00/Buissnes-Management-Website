fd0a786108c0e274d3652474c0e7fd991b20f20d
GK <gabykassab0@gmail.com>
2025-02-07 19:47:45 +0200

    adding image

--- frontend/src/app/components/paymentgateway/Paymentpart.jsx
+++ frontend/src/app/components/paymentgateway/Paymentpart.jsx
@@ -2,7 +2,7 @@
 import { useInView } from 'framer-motion'
 import React, { useRef } from 'react'
 import Image from 'next/image';
-import gateway1 from './../../public/images/gateway1.webp'
+import gateway2 from './../../public/images/gateway2-bg.webp'
 import avatar3 from './../../public/images/avatar3.webp'
 import Link from 'next/link';
 import * as Icon from '@phosphor-icons/react/dist/ssr'
@@ -28,12 +28,19 @@ const Paymentpart = () => {
                         </div>
 
                         <div className='button block'>
-                        <Link className='button-main box-shadow bg-blue-700 hover:bg-black text-white bg-blue whitespace-nowrap rounded-full' href='/'>
+                        <Link className='button-main box-shadow bg-black hover:bg-black text-white bg-blue whitespace-nowrap rounded-full' href='/'>
                         Get Started 
-                        
                         </Link>
                         </div>
 
+                </div>
+
+                <div className='w-11/12 xl:w-7/12'>
+                <div className='right pl-10' style={{transform:isInview ? "none" : 'translateY(60px)', opacity:isInview?1:0,transition:'all 0.7s cubic-bezier(0.17,0.55,0.55,1) 0.3s' }}>
+                    <div className='bg-img '>
+                        <Image width={5000} height={5000} className='w-full' src={gateway2} alt='image'/>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
