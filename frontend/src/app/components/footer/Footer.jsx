c094213acbfa131d4cb6fca0b99f24fbb4d7f0e9
GK <gabykassab0@gmail.com>
2025-02-08 13:04:14 +0200

    adding first column of links

--- frontend/src/app/components/footer/Footer.jsx
+++ frontend/src/app/components/footer/Footer.jsx
@@ -17,23 +17,61 @@ const Footer = () => {
 
             <div className='list-social flex items-center gap-2 '>
                 <Link className='item rounded-full w-7 h-7 border-2 border-gray flex items-center justify-center' href='https://facebook.com' target='_blank'>
-                <i className='icon-facebook text-sm'></i>
+                <i className='icon-facebook text-xs'></i>
                 </Link>
 
                 <Link className='item rounded-full w-7 h-7 border-2 border-gray flex items-center justify-center' href='https://linkedin.com' target='_blank'>
-                <i className='icon-in text-sm'></i>
+                <i className='icon-in text-xs'></i>
                 </Link>
 
                 <Link className='item rounded-full w-7 h-7 border-2 border-gray flex items-center justify-center' href='https://twitter.com' target='_blank'>
-                <i className='icon-twitter text-sm'></i>
+                <i className='icon-twitter text-xs'></i>
                 </Link>
 
                 <Link className='item rounded-full w-7 h-7 border-2 border-gray flex items-center justify-center' href='https://youtube.com' target='_blank'>
-                <i className='icon-youtube text-sm'></i>
+                <i className='icon-youtube text-xs'></i>
                 </Link>
             </div>
         </div>
 
+        </div>
+
+        <div className='lg:w-1/2'>
+        <div className='footer-navigate flex items-center justify-center gap-20'>
+            <div className='footer-nav-item'>
+
+            <div className='item-heading text-button-sm text-white'>
+                Quick Links
+            </div>
+            <ul className='list-nav mt-1 text-white'>
+                <li className='mt-3'>
+                    <Link className='caption1 has-line-before line-white text-surface hover-underline' href='/'>
+                        About us 
+                    </Link>
+                    
+                </li>
+                <li className='mt-3'>
+                    <Link className='caption1 has-line-before line-white text-surface hover-underline' href='/'>
+                        Services
+                    </Link>
+                    
+                </li>
+                <li className='mt-3'>
+                    <Link className='caption1 has-line-before line-white text-surface hover-underline' href='/'>
+                        Case Studies
+                    </Link>
+                    
+                </li>
+                <li className='mt-3'>
+                    <Link className='caption1 has-line-before line-white text-surface hover-underline' href='/'>
+                        contact
+                    </Link>
+                    
+                </li>
+            </ul>
+            </div>
+        </div>
+        
         </div>
         </div>
       </div>
