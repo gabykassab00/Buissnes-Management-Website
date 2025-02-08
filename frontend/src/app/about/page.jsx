a79ee61b5ee95b83d9f978e2816960996426be31
GK <gabykassab0@gmail.com>
2025-02-08 16:14:59 +0200

    adding service part

--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -7,6 +7,9 @@ import Breadcrumb from '../components/section/Breadcrumb'
 import header from '../../../public/header.webp'
 import Aboutsection from '../components/section/Aboutsection'
 import Counter from '../components/section/Counter'
+import Service from '../components/service/Service'
+import serviceData from '@/./app/data/service.json'
+
 const Page = () => {
   return (
 <div className="overflow-x-hidden">
@@ -18,6 +21,7 @@ const Page = () => {
       <Breadcrumb link="about us" img={header} title="about us" desc="the jobs report soundly beat expectations , with job gains broadly spread across the economy and about 60% higher "/>
     <Aboutsection/>
     <Counter className="lg:pb-[50px] sm:pb-16 pb-10"/>
+    <Service data={serviceData} className="pb-10"/>
     </main>
     <Partner className='lg:mt-[100px] sm:mt-16 mt-10'/>
     <footer id="footer">
--- frontend/src/app/components/section/Counter.jsx
+++ frontend/src/app/components/section/Counter.jsx
@@ -18,6 +18,46 @@ const Counter = ({className}) => {
                         </div>
                     </div>
                 </div>
+
+                <div className='item'>
+                    <div className='flex flex-col items-center'>
+                        <div className='count-block flex items-center'>
+                            <div className='counter heading3 '>
+                                1.77
+                            </div>
+                            <span className='heading3'>K</span>
+                        </div>
+                        <div className='body1 text-secondary text-center'>
+                            Buissness Problem Solving
+                        </div>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='flex flex-col items-center'>
+                        <div className='count-block flex items-center'>
+                            <div className='counter heading3 '>
+                                298
+                            </div>
+                            <span className='heading3'>K</span>
+                        </div>
+                        <div className='body1 text-secondary text-center'>
+                            Goal achiever
+                        </div>
+                    </div>
+                </div>
+                <div className='item'>
+                    <div className='flex flex-col items-center'>
+                        <div className='count-block flex items-center'>
+                            <div className='counter heading3 '>
+                                246
+                            </div>
+                            <span className='heading3'>K</span>
+                        </div>
+                        <div className='body1 text-secondary text-center'>
+                            passive income earners
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
