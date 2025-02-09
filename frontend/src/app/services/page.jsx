ac840ef4dd62fb6d67ff050563c44cda2a696150
GK <gabykassab0@gmail.com>
2025-02-09 12:35:56 +0200

    adding hero section

--- frontend/src/app/services/page.jsx
+++ frontend/src/app/services/page.jsx
@@ -5,10 +5,10 @@ import Partner from '../components/partner/Partner'
 import Footer from '../components/footer/Footer'
 import Breadcrumb from '../components/section/Breadcrumb'
 import header from '../../../public/header.webp'
-import Aboutsection from '../components/section/Aboutsection'
-import Counter from '../components/section/Counter'
 import Service from '../components/service/Service'
 import serviceData from '@/./app/data/service.json'
+import Image from 'next/image'
+import assessment from './../../../../frontend/public/assessment.webp'
 
 const Page = () => {
   return (
@@ -18,9 +18,30 @@ const Page = () => {
       <Menu/>
     </header>
     <main className='content'>
-      <Breadcrumb link="about us" img={header} title="about us" desc="the jobs report soundly beat expectations , with job gains broadly spread across the economy and about 60% higher "/>
-    <Aboutsection/>
-    <Counter className="lg:pb-[50px] sm:pb-16 pb-10"/>
+      <Breadcrumb link="our services" img={header} title="our services" desc="the jobs report soundly beat expectations , with job gains broadly spread across the economy and about 60% higher "/>
+    <div className='mt-[100px]'>
+      <div className='container'>
+        <div className='flex gap-8 max-lg:flex-col-reverse'>
+          <div className='w-full lg:w-1/2 flex flex-col justify-between gap-5 pr-10'>
+          <div className='heading3'>
+            Credit Card Management Use Wisely
+            <div className='body2 text-secondary mt-4'>
+              we offer reliable risk managment services to safeguard your assests,ensure financial stability ,and navigate rapildy changing buissnes environments. we understand that managing risks is crucial for the sustainability and sucess of your enterprise
+            </div>
+          </div>
+          </div>
+
+          <div className='w-full lg:w-1/2'>
+            <div className='bg-img w-full overflow-hidden rounded-xl'>
+
+              <Image width={5000} height={5000} className='w-full h-full block' src={assessment} alt='img'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
     <Service data={serviceData} className="pb-10"/>
     </main>
     <Partner className='lg:mt-[100px] sm:mt-16 mt-10'/>
